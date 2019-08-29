7e20964d90d0317cf6356175f8719b473177b81e
Jas Spencer <jas.spencer414@gmail.com>
2019-08-29 16:11:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,9 +8,22 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-
+  max = 0
+     array.length.times do |count|
+       if array[count] > max
+         max = array[count]
+       end
+     end
+   max
 end
 
 def find_min_value(array)
-
+  min = 0
+      array.length.times do |count|
+        if count == 0
+          min = array[count]
+        else
+          if array[count] < min
+            min = array[count]
+          end
 end
